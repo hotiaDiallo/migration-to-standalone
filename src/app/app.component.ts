02faf979cd67220fcda737134228986b3028fc5f
hotiaDiallo <ibrahimahotia.diallo@gmail.com>
2023-12-03 09:12:08 +0100

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
       <header class="header">
         <img src="/assets/img/logo.svg" alt="Ultimate Donuts" class="logo">
       </header>
-      <donut-list></donut-list>
+      <router-outlet></router-outlet>
     </div> 
   `,
   styles: [
